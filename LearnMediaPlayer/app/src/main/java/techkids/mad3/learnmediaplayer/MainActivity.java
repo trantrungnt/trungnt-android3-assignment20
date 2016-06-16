e8c844cef9326e6264ddb924b5b03ca355739a12
Trần Trung <nguyentrantrung1986@gmail.com>
2016-06-17 03:55:00 +0700

    define initComponents method (temp)

--- LearnMediaPlayer/app/src/main/java/techkids/mad3/learnmediaplayer/MainActivity.java
+++ LearnMediaPlayer/app/src/main/java/techkids/mad3/learnmediaplayer/MainActivity.java
@@ -2,12 +2,45 @@ package techkids.mad3.learnmediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnReplay, btnAction, btnPrevious, btnNext, btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        btnReplay = (Button) this.findViewById(R.id.btnReplay);
+        btnAction = (Button) this.findViewById(R.id.btnAction);
+        btnPrevious = (Button) this.findViewById(R.id.btnBack);
+        btnNext = (Button) this.findViewById(R.id.btnNext);
+
+        btnReplay.setOnClickListener(this);
+        btnAction.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+        btnPrevious.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id)
+        {
+            case R.id.btnReplay:
+                break;
+            case R.id.btnAction:
+                break;
+            case R.id.btnBack:
+                break;
+            case R.id.btnNext:
+                break;
+        }
     }
 }
