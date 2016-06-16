fccce671b3a2ad1814d7c29b7c365bd46761724f
Trần Trung <nguyentrantrung1986@gmail.com>
2016-06-17 04:15:13 +0700

    define displaySongList method (temp)

--- LearnMediaPlayer/app/src/main/java/techkids/mad3/learnmediaplayer/MainActivity.java
+++ LearnMediaPlayer/app/src/main/java/techkids/mad3/learnmediaplayer/MainActivity.java
@@ -1,18 +1,44 @@
 package techkids.mad3.learnmediaplayer;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnReplay, btnAction, btnPrevious, btnNext, btnStop;
+    private ListView lvListFileMusic;
+    private ArrayList<Song> arrSongList;
+    private ContentResolver musicResolver;
+    private Uri musicUri;
+    private Cursor musicCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponents();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        displaySongList();
+    }
+
+    private void displaySongList() {
+        arrSongList = new ArrayList<Song>();
+        musicResolver = getContentResolver();
+        musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        musicCursor = musicResolver.query(musicUri, null, null, null, null);
     }
 
     private void initComponents() {
@@ -25,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAction.setOnClickListener(this);
         btnNext.setOnClickListener(this);
         btnPrevious.setOnClickListener(this);
+
+        lvListFileMusic = (ListView) this.findViewById(R.id.lvListFileMusic);
     }
 
     @Override
---
+++ LearnMediaPlayer/app/src/main/java/techkids/mad3/learnmediaplayer/Song.java
@@ -0,0 +1,53 @@
+package techkids.mad3.learnmediaplayer;
+
+/**
+ * Created by TrungNT on 6/17/2016.
+ */
+public class Song {
+    private long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    private String title;
+    private String artist;
+    private String currentDate;
+    private String time;
+}
